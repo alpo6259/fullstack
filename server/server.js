3dbaecd4898e4b97fbaa9113a3874de1c567935f
alpo6259 <alpo6259@gmail.com>
2022-08-10 16:53:25 +0300

    fix connections

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const controller = require('./socketInit');
 require('./dbMongo/mongoose');
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 server.listen(PORT, () =>
