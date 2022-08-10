dc6cb0ce5d6d649cbd42f8fb5356a724aa9d314e
alpo6259 <alpo6259@gmail.com>
2022-08-10 18:28:28 +0300

    refactor mongo model

--- server/models/mongoModels/index.js
+++ server/models/mongoModels/index.js
@@ -1,8 +1,10 @@
+const fs = require('fs');
 const mongoose = require('mongoose');
 const path = require('path');
 const env = process.env.NODE_ENV || 'development';
-const configPath = path.join(__dirname, '..', 'config/mongoConfig.json');
+const configPath = path.join(__dirname, '..', '..', 'config/mongoConfig.json');
 const config = require(configPath)[env];
+const basename = path.basename(__filename);
 
 mongoose.connect(
   `mongodb://${config.host}:${config.port}/${config.database}`,
@@ -14,7 +16,21 @@ mongoose.connect(
     }
   },
 );
+const db = {};
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    );
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file));
+    db[model.vodelName] = model;
+  });
 
 mongoose.set('debug', env === 'development');
+
+db.mongoose = mongoose;
 
 module.exports = mongoose;
